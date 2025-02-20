65a1497f41e308b5354e47f02ec73803fbb6639b
Saylorer <creatoru@mail.ru>
2025-02-20 20:41:33 +0300

    20.02.15 Добавил главную страницу, набросал иконок для сайта, сделал фундамент к каталогу

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
+    'goods',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- app/main/views.py
+++ app/main/views.py
@@ -4,10 +4,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) -> HttpResponse:
     context = {
-        'title': 'Home',
-        'content': 'Welcome'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
 
     }
+
     return render(request, 'main/index.html',context)
-def about(request) -> HttpResponse:
-    return HttpResponse('About')
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
+
+    return render(request, 'main/about.html', context)
