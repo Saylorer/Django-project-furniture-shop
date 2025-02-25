b51f7ec37ff4cf32a83eb5c712f6df137afbf4c9
Saylorer <creatoru@mail.ru>
2025-02-25 17:49:19 +0300

    25.02.25 Доделал поиск

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,6 +1,6 @@
 from pydoc import describe
 
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 from django.db.models import Q
 from goods.models import Products
 
@@ -13,7 +13,29 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
 
     # keywords = [word for word in query.split() if len(word) > 2]
     #
